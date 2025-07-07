583a243a4027378e909e67b6dbdb1f1367d4b6fc
grilldemo <ryanmcnicholas7@gmail.com>
2025-07-07 11:39:15 -0400

    javafx and sql driver installed

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+import database.DatabaseObject;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new DatabaseObject();
+	}
+
+}
---
+++ src/database/DatabaseObject.java
@@ -0,0 +1,35 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+
+public class DatabaseObject {
+
+	Connection connecter; 
+	Statement statement;
+	public DatabaseObject() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("driver loaded");
+			connecter=DriverManager.getConnection("jdbc:mysql://localhost:3306/joes_grill","root","1234");
+			System.out.println("database loaded");
+			statement=connecter.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+			System.out.println("Statement created");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	
+}
